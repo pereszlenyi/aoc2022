6df49cf17787c926c89f4752ad4248e4f047ef00
Attila Pereszlényi <attila@pereszlenyi.com>
2023-01-29 23:58:15 +0100

    Script for installing all the requirements. Only meant to work with Ubuntu.

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Run this script to install all the compilers and interpreters
+# required to build and run the solutions.
+
+ECHO=/usr/bin/echo
+SUDO=/usr/bin/sudo
+DIRNAME=/usr/bin/dirname
+LS=/usr/bin/ls
+APTGET=/usr/bin/apt-get
+ANSIBLE=/usr/bin/ansible-playbook
+
+function die {
+	$ECHO "Error: $1"
+	exit 1
+}
+
+for FILE in "$ECHO" "$SUDO" "$DIRNAME" "$LS" "$APTGET" ; do
+	[ -x "$FILE" ] || die "'$FILE' doesn't exist or not executable."
+done
+
+function check_ansible {
+	[ -x "$ANSIBLE" ]
+}
+
+DIR=$($DIRNAME "$0")
+cd $DIR || die "Can't enter $DIR."
+$LS ./install.ansible.yml >/dev/null || die "Ansible playbook is missing."
+
+check_ansible || ($ECHO "Installing ansible:" && $SUDO $APTGET install ansible && $ECHO "")
+check_ansible || die "Ansible is not installed."
+
+$ANSIBLE ./install.ansible.yml --ask-become-pass && \
+$ECHO "Install was successful." || \
+die "Install failed."
